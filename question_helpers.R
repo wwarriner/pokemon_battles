3491a64bf7d181fc294d78dc0cd2ffecbffdc9ab
UAB\wwarr <wwarr@uab.edu>
2018-10-17 16:11:50 -0500

    update

--- question_helpers.R
+++ question_helpers.R
@@ -14,3 +14,17 @@ combine_types <- function(pokemon_combat_summary) {
     gather(which_type, type, type.1:type.2) %>%
     filter(type!="")
 }
+
+summarize_stats <- function(data) {
+  combine_stats(data) %>%
+    group_by(stat) %>%
+    summarize(min=min(value),
+              q10=quantile(value,probs=0.10),
+              q25=quantile(value,probs=0.25),
+              median=median(value),
+              mean=mean(value),
+              sd=sd(value),
+              q75=quantile(value,probs=0.75),
+              q90=quantile(value,probs=0.90),
+              max=max(value))
+}
--- questions.R
+++ questions.R
@@ -2,62 +2,148 @@ source("import_helpers.R")
 library(ggplot2)
 
 # How many unique pokemon ids are there?
-q1 <- function(pokemon_data) {
+q1.1 <- function(pokemon_data) {
   id_unique_count <- length(unique(pokemon_data$id))
-  sprintf("There are %d unique pokemon ids in the dataset.\n", id_unique_count)
+  print(sprintf("There are %d unique pokemon ids in the dataset.", id_unique_count))
   id_unique_count
 }
 
 # How are they distributed by first type?
-q1.1 <- function(pokemon_data) {
+q1.2 <- function(pokemon_data, color_data) {
   type_counts <-
     pokemon_data %>%
     group_by(type.1) %>%
-    summarize(n())
+    summarize(count=n())
+  print(type_counts)
+  write.csv(type_counts, file="q1.2.csv")
+  g <- ggplot(type_counts, aes(x=type.1, y=count)) +
+    geom_col(aes(fill=type.1)) +
+    scale_color_manual(values=color_data, aesthetics="fill") +
+    theme(axis.text.x = element_text(angle=90, hjust=1))
+  print(g)
+  ggsave("q1.2.png")
+}
 
+# How are they distributed by second type?
+q1.3 <- function(pokemon_data, color_data) {
+  type_counts <-
+    pokemon_data %>%
+    group_by(type.2) %>%
+    summarize(count=n())
+  print(type_counts)
+  write.csv(type_counts, file="q1.3.csv")
+  color_data <- append(color_data, "#444444")
+  g <- ggplot(type_counts, aes(x=type.2, y=count)) +
+    geom_col(aes(fill=type.2)) +
+    scale_color_manual(values=color_data, aesthetics="fill") +
+    theme(axis.text.x = element_text(angle=90, hjust=1))
+  print(g)
+  ggsave("q1.3.png")
+}
+
+# What percent of pokemon have two types?
+q1.4 <- function(pokemon_data) {
+  two_types_count <- sum(pokemon_data$type.2!="")
+  ratio <- two_types_count/q1.1(pokemon_data)
+  percent <- ratio*100
+  print(sprintf("%.2f%% of pokemon have two types.", percent))
+  ratio
 }
 
 # How frequently are types paired?
-q1.2 <- function(pokemon_data) {
+q1.5 <- function(pokemon_data) {
   two_types <-
     pokemon_data %>%
-    filter(type.2!="") %>%
-    mutate(type.2=droplevels(type.2)) %>%
     group_by(type.1, type.2) %>%
     summarize(count=n()) %>%
+    ungroup() %>%
     complete(type.1, type.2, fill=list(count=0))
   g <- ggplot(two_types, aes(x=type.1, y=type.2)) +
     geom_tile(aes(fill=count)) +
     geom_text(aes(label=count)) +
-    scale_fill_gradient2(low="white",
-                         high="midnightblue") +
+    scale_fill_gradientn(colors=c("white", "lightskyblue", "springgreen4"),
+                         values=scales::rescale(c(0,0.01,1.0))) +
     theme(axis.text.x=element_text(angle=90))
   print(g)
+  ggsave("q1.5.png")
+}
+
+# Box plot of stats overall
+q1.6 <- function(pokemon_data) {
+  box_plot_data <-
+    pokemon_data %>%
+    combine_stats()
+  g <- ggplot(box_plot_data, aes(x=stat, y=value, fill=stat)) +
+    geom_boxplot()
+  print(g)
+  ggsave("q1.6.png")
+}
+
+# Summary data of stats
+q1.7 <- function(pokemon_data) {
+  summary_data <-
+    pokemon_data %>%
+    summarize_stats() %>%
+    mutate_at(.vars=vars(mean, sd), .funs=function(x){sprintf("%.2f",round(x,2))})
+  print(summary_data)
+  write.csv(summary_data, file="q1.7.csv")
+}
+
+# What percent of pokemon are legendary?
+q1.8 <- function(pokemon_data) {
+  legendary_data <-
+    pokemon_data %>%
+    group_by(legendary) %>%
+    summarize(count=n())
+  legendary_count <- sum(legendary_data=="True")
+  ratio <- legendary_count/q1.1(pokemon_data)
+  percent <- ratio*100
+  print(sprintf("%.2f%% of pokemon have two types.", percent))
+  print(ratio)
+  g <- ggplot(legendary_data, aes(x=legendary, y=count)) +
+    geom_col()
+  print(g)
+  ggsave("q1.8.png")
+}
+
+# What do stats look like broken out by legendary status?
+q1.9 <- function(pokemon_data) {
+  box_plot_data <-
+    pokemon_data %>%
+    group_by(legendary) %>%
+    combine_stats()
+  g <- ggplot(box_plot_data, aes(x=stat, y=value, fill=legendary)) +
+    geom_boxplot()
+  print(g)
+  ggsave("q1.9.png")
 }
 
 # What does the left-side-wins space look like?
 q2 <- function(combat_left_side_wins_table) {
   par(pty="s")
+  png("q2.png")
   image(combat_left_side_wins_table,
         useRaster=TRUE,
         xaxt="none",
-        yaxt="none")
+        yaxt="none",
+        xlab="Left Side ID",
+        ylab="Right Side ID")
   x <- ncol(combat_left_side_wins_table)
   at <- seq(0.0, 1.0, 0.125)
   labels <- as.character(seq(0, x, round(x/8)))
   axis(1, at=at, labels=labels)
   axis(2, at=at, labels=labels)
-  png("q2.png")
+  dev.off()
 }
 
 # What fraction of coverage of the space is there in the dataset?
-q3 <- function(pokemon_data,combat_data) {
+q3 <- function(pokemon_data, combat_data) {
   all_uids <- q1(pokemon_data)
   population_count <- all_uids*all_uids
   sample_count <- nrow(combat_data)
   coverage_ratio <- sample_count/population_count
   coverage_percent <- coverage_ratio*100
-  sprintf("The coverage ratio of the population space is %.2f%%\n", coverage_percent)
+  print(sprintf("The coverage ratio of the population space is %.2f%%\n", coverage_percent))
   coverage_ratio
 }
 
@@ -73,22 +159,22 @@ q4 <- function(pokemon_data,combat_data,combat_left_side_wins_table) {
 # legendary-fighting, just ahead of legendary-grass and legendary-normal
 # of non-legendary, it is flying, then dark, then electric
 q5 <- function(pokemon_combat_summary, color_data) {
-  labels <- c("False"="Nonlegendary",
-              "True"="Legendary")
-  d <-
+  wins_appears_data <-
     pokemon_combat_summary %>%
     combine_types() %>%
     mutate(type=factor(type)) %>%
     group_by(legendary, type) %>%
     summarize(wins=sum(wins), appears=sum(appears))
+  labels <- c("False"="Nonlegendary",
+              "True"="Legendary")
   g <-
-    ggplot(d,
+    ggplot(wins_appears_data,
            aes(x=type,
                y=wins/appears)) +
     facet_grid(cols=vars(legendary),
                labeller=as_labeller(labels)) +
     geom_col(aes(fill=type)) +
-    scale_fill_manual(values=colors) +
+    scale_fill_manual(values=color_data) +
     scale_y_continuous(label=scales::percent) +
     coord_cartesian(ylim=c(0, 1)) +
     theme(axis.text.x = element_text(angle=60, hjust=1))
@@ -130,7 +216,10 @@ q7 <- function(pokemon_combat_summary, color_data) {
     geom_smooth(method='lm', formula=y~x) +
     scale_color_manual(values=color_data) +
     scale_y_continuous(label=scales::percent) +
-    coord_cartesian(ylim=c(0, 1))
+    coord_cartesian(ylim=c(0, 1)) +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())
   print(g)
   ggsave("q7.png")
 }
@@ -141,7 +230,8 @@ q8 <- function(combat_features) {
     combat_features %>%
     select(left_side_won,hp_diff:speed_diff) %>%
     gather(key=stat,value=diff,hp_diff:speed_diff) %>%
-    mutate(diff=as.numeric(diff))
+    mutate(diff=as.numeric(diff)) %>%
+    mutate(left_side_won=as.logical(left_side_won))
   g <-
     ggplot(d,
            aes(x=diff,
